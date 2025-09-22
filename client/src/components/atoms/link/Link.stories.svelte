2edcd4c5d0a7d91990b1f2e6470bab0512edd346
Edanriell <Edanriell@gmail.com>
2025-09-22 14:11:20 +0300

    feature(link-extended): make link more versatile

--- client/src/components/atoms/link/Link.stories.svelte
+++ client/src/components/atoms/link/Link.stories.svelte
@@ -1,5 +1,6 @@
 <script lang="ts">
 	import {defineMeta} from "@storybook/addon-svelte-csf";
+
 	import Link from "./Link.svelte";
 
 	const { Story } = defineMeta({
@@ -14,35 +15,58 @@
 				control: "text",
 				description: "The URL the link should navigate to",
 			},
-			children: {
+			textColor: {
+				control: "color",
+				description: "Link text color",
+			},
+			textHoverColor: {
+				control: "color",
+				description: "Link text color on hover",
+			},
+			classes: {
 				control: "text",
-				description: "Slot content for the link",
+				description: "Additional CSS classes",
 			},
 		},
 		args: {
-			link: "#"
+			link: "#",
+			textColor: "#575989",
+			textHoverColor: "#575989",
+			classes: "",
 		},
 	});
 </script>
 
+<!-- Default link -->
 <Story name="Default">Default Link</Story>
 
-<Story args={{ link: 'https://example.com' }} name="WithCustomUrl">
+<!-- Link with custom URL -->
+<Story args={{ link: "https://example.com" }} name="External Link">
 	Visit Example.com
 </Story>
 
-<Story args={{ link: '/about' }} name="InternalLink">
+<!-- Internal link -->
+<Story args={{ link: "/about" }} name="Internal Link">
 	About Us
 </Story>
 
-<Story args={{ link: '#' }} name="LongText">
+<!-- Custom text colors -->
+<Story
+		args={{
+			textColor: "#2d67f3",
+			textHoverColor: "#6f9bfa"
+		}}
+		name="Custom Colors"
+>
+	Colored Link
+</Story>
+
+<!-- Additional classes -->
+<Story args={{ classes: "underline font-bold" }} name="Underlined Bold">
+	Underlined Bold Link
+</Story>
+
+<!-- Long text -->
+<Story name="Long Text">
 	This is a very long link text that demonstrates how the component handles longer content
-</Story>
-
-<Story args={{ link: '#' }} name="ShortText">
-	Go
-</Story>
-
-<Story name="EmptyHref">
-	Default Anchor Link
 </Story>
