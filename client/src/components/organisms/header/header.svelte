6c42d11119f6387701990cb62aa3ce6f60324fa2
Edanriell <Edanriell@gmail.com>
2025-09-26 09:42:24 +0300

    feature(header): implemented

--- client/src/components/organisms/header/header.svelte
+++ client/src/components/organisms/header/header.svelte
@@ -1,9 +1,19 @@
 <script lang="ts">
 	import {Link, Logotype} from "../../atoms";
 	import {PrimaryNavigation} from "../../molecules";
+
+	interface Props {
+		classes?: string;
+		[key: string]: unknown;
+	}
+
+	let {
+		classes = "",
+		...restProps
+	}: Props = $props();
 </script>
 
-<header class="flex flex-row items-center justify-between pl-[30rem] pr-[30rem] mt-[30rem] mb-[30rem] desktop:pr-[90rem] desktop:pl-[90rem] desktop:mt-[90rem] desktop:mb-[90rem]">
+<header {...restProps} class="flex flex-row items-center justify-between pl-[30rem] pr-[30rem] mt-[30rem] mb-[30rem] desktop:pr-[90rem] desktop:pl-[90rem] desktop:mt-[90rem] desktop:mb-[90rem] {classes}">
 	<Logotype colorScheme="Dark" />
 	<PrimaryNavigation name="Primary navigation" >
 		<Link url="#">Features</Link>,
