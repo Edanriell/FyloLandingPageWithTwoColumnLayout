434d2bdcb8628794beebf194b8e67fb008499af0
Edanriell <Edanriell@gmail.com>
2025-10-02 10:34:12 +0300

    bugfix(component-fix): fixed header and logotype

--- Client/src/components/organisms/header/Header.svelte
+++ Client/src/components/organisms/header/Header.svelte
@@ -14,7 +14,7 @@
 	}: Props = $props();
 </script>
  
-<header {...restProps} class="flex flex-row items-center justify-between pl-[30rem] pr-[30rem] mt-[30rem] mb-[30rem] desktop:pr-[90rem] desktop:pl-[90rem] desktop:mt-[90rem] desktop:mb-[90rem] {classes}">
+<header {...restProps} class="flex flex-row items-center justify-between pl-[30rem] pr-[30rem] mt-[30rem]! mb-[30rem]! desktop:pr-[90rem] desktop:pl-[90rem] desktop:mt-[90rem]! desktop:mb-[90rem]! {classes}">
 	<Logotype colorScheme="Dark" />
 	<PrimaryNavigation>
 		<PrimaryNavigationItem>
