fd125a2e35f35e61e0a8f41d94a483f0620a98c0
Edanriell <Edanriell@gmail.com>
2025-10-13 22:38:12 +0300

    feature(secondary-header): created component

---
+++ Client/src/components/atoms/secondary-heading/SecondaryHeading.svelte
@@ -0,0 +1,19 @@
+<script lang="ts">
+	import type {Snippet} from "svelte";
+
+	interface Props {
+		classes?: string;
+		children?: Snippet;
+		[key: string]: unknown;
+	}
+
+	let {
+		children,
+		classes = "",
+		...restProps
+	}: Props = $props();
+</script>
+
+<h2 {...restProps} class="font-[Raleway] text-[#07043b] text-center font-bold leading-[150%] text-[16rem] text-pretty! desktop:text-[40rem] desktop:leading-none {classes}">
+	{@render children?.()}
+</h2>
--- Client/src/components/organisms/about-section/AboutSection.svelte
+++ Client/src/components/organisms/about-section/AboutSection.svelte
@@ -1,5 +1,5 @@
 <script lang="ts">
-	import {AboutIllustration, Text} from "@atoms";
+	import {AboutIllustration, Link, SecondaryHeading, Text} from "@atoms";
 
 	interface Props {
 		classes?: string;
@@ -14,12 +14,12 @@
 
 <section {...restProps} class="flex flex-col-reverse items-center {classes}">
 	<div class="relative">
-		<h2>Stay productive, wherever you are</h2>
+		<SecondaryHeading>Stay productive, wherever you are</SecondaryHeading>
 		<Text classes="text-[#615f7e]! desktop:text-left desktop:text-[16rem]! desktop:text-[#07043b]! desktop:leading-[150%]! desktop:tracking-normal!">Never let location be an issue when accessing your files. Fylo has you covered for all of your file storage needs.</Text>
 		<Text classes="text-[#615f7e]! desktop:text-left desktop:text-[16rem]! desktop:text-[#07043b]! desktop:leading-[150%]! desktop:tracking-normal!">Securely share files and folders with friends, family and colleagues for live collaboration. No email attachments required!</Text>
-		<a href="#">
+		<Link href="#">
 			<span>See how Fylo works</span>
-		</a>
+		</Link>
 		<figure>
 			<blockquote>
 				Fylo has improved our team productivity by an order of magnitude. Since making the switch our team has become a well-oiled collaboration machine.
