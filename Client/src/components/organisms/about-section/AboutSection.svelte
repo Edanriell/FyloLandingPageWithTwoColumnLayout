df886050433316af377136b7a6df8bafac11bee2
Edanriell <Edanriell@gmail.com>
2025-10-13 20:36:37 +0300

    feature(about-section): created about section

---
+++ Client/src/components/organisms/about-section/AboutSection.svelte
@@ -0,0 +1,26 @@
+<script lang="ts">
+</script>
+
+<section>
+	<div>
+		<h2>Stay productive, wherever you are</h2>
+		<p>Never let location be an issue when accessing your files. Fylo has you covered for all of your file storage needs.</p>
+		<p>Securely share files and folders with friends, family and colleagues for live collaboration. No email attachments required!</p>
+		<a href="#">
+			<span>See how Fylo works</span>
+		</a>
+		<figure>
+			<blockquote>
+				Fylo has improved our team productivity by an order of magnitude. Since making the switch our team has become a well-oiled collaboration machine.
+			</blockquote>
+			<figcaption>
+				<img alt="Portrait of Kyle Burton, Founder & CEO of Huddle" src="">
+				<div>
+					<strong>Kyle Burton</strong>
+					<small>Founder & CEO, Huddle</small>
+				</div>
+			</figcaption>
+		</figure>
+	</div>
+	<div></div>
+</section>
