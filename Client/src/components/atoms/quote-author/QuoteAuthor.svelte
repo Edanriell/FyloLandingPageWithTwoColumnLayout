add5dc4a9559b1d3063c3382830facdd15187c92
Edanriell <Edanriell@gmail.com>
2025-10-14 15:10:46 +0300

    feature(quote-block-styles): created quote author styles

--- Client/src/components/atoms/quote-author/QuoteAuthor.svelte
+++ Client/src/components/atoms/quote-author/QuoteAuthor.svelte
@@ -16,10 +16,10 @@
 	}: Props = $props();
 </script>
 
-<figcaption {...restProps} class=" {classes}">
-	<img alt="Portrait of Kyle Burton, Founder & CEO of Huddle" src="{authorImageUrl}">
-	<div class="flex flex-col">
-		<strong>{authorFullName}</strong>
-		<small>{authorJobTitle}</small>
+<figcaption {...restProps} class="flex flex-row items-center gap-x-[7.46rem] {classes}">
+	<img alt="Portrait of Kyle Burton, Founder & CEO of Huddle" class="rounded-full object-contain w-[24rem] h-[24rem] desktop:w-[32rem] desktop:h-[32rem]" src="{authorImageUrl}">
+	<div class="flex flex-col items-start gap-y-[1.8rem] desktop:gap-y-[8rem] desktop:gap-y-[unset]">
+		<strong class="font-[OpenSans]! font-bold! text-[9rem]! tracking-[0.05em] text-[#07043b] desktop:text-[11rem]!">{authorFullName}</strong>
+		<small class="font-[OpenSans]! font-normal! text-[6rem]! leading-[197%] tracking-[0.05em] text-[#07043b] desktop:text-[8rem]! desktop:leading-[187%]">{authorJobTitle}</small>
 	</div>
 </figcaption>
