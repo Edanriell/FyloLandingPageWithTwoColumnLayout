281bf563c2ff1053a43fcf214c3a4ce4649d8167
Edanriell <Edanriell@gmail.com>
2025-10-14 13:34:37 +0300

    feature(quote-author): created atom

--- Client/src/components/atoms/quote-author/QuoteAuthor.svelte
+++ Client/src/components/atoms/quote-author/QuoteAuthor.svelte
@@ -1,1 +1,25 @@
+<script lang="ts">
+	interface Props {
+		authorImageUrl?: string;
+		authorFullName: string;
+		authorJobTitle: string;
+		classes?: string;
+		[key: string]: unknown;
+	}
 
+	let {
+		authorImageUrl = "",
+		authorFullName,
+		authorJobTitle,
+		classes = "",
+		...restProps
+	}: Props = $props();
+</script>
+
+<figcaption {...restProps} class=" {classes}">
+	<img alt="Portrait of Kyle Burton, Founder & CEO of Huddle" src="{authorImageUrl}">
+	<div class="flex flex-col">
+		<strong>{authorFullName}</strong>
+		<small>{authorJobTitle}</small>
+	</div>
+</figcaption>
